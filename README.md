adb31f39a631d2c7ebd26623d57609814e72e521
Ericlee1783 <ericlee1783@gmail.com>
2023-10-09 22:39:01 -0700

    save point

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SVG-Logo-Maker
 
 ## Description
+This project is to build a logo using the inquirer prompts and for the shapes to be in different colors or shapes depending on what the user types in. 
+
+## Installation
+node index.js and it should launch the commands.
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
 inquirer
 .prompt([
     {
@@ -22,6 +23,19 @@ inquirer
     {
         type: 'input',
         message: 'Enter a shape color',
-        name: 'shape color',
+        name: 'shapeColor',
     }
 ])
+.then((response) => {
+    const svgResult = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <circle cx="150" cy="100" r="80" fill="${response.shapeColor}" />
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.color}">${response.text}</text>
+  
+  </svg>`;
+  fs.writeFile('result.svg', svgResult, (err) => {
+    if (err) throw err;
+    console.log("Circle Created!")
+  });
+});
